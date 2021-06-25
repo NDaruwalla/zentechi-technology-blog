a6451ddb56b9bdc2e4b635001e42b45fe7af6bfa
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-06-25 17:03:35 -0400

    began server.js with required modules

---
+++ server.js
@@ -0,0 +1,7 @@
+// these are the modules required for this app
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
