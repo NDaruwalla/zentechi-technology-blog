9a263b5e7d43d2fab4bd58f394299638865bd3a8
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-06-25 18:52:59 -0400

    created post model

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+// require sequelize and config/connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+// create/define columns for post model (id, title, post content, and user id)
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      post_content: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+
+  module.exports = Post;
